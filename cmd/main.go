d2712950d6d3aa003d578c3afdddb39034f96c4b
dengkai <1756606419@qq.com>
2026-02-28 14:28:16 +0800

    注释测试案例

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"main/agent"
 	"main/config"
-	"main/demo_rag"
 	"main/graph/export_graph"
 	"main/internal/database"
 	"main/internal/repository"
@@ -13,9 +12,6 @@ import (
 	"time"
 
 	_ "net/http/pprof"
-
-	"github.com/cloudwego/eino/components/document"
-	uuid2 "github.com/google/uuid"
 )
 
 const (
@@ -50,59 +46,59 @@ func loggingMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-func save(ctx context.Context) {
-	r, err := demo_rag.InitRAGEngine(ctx, index, prefix)
-	if err != nil {
-		panic(err)
-	}
-
-	doc, err := r.Loader.Load(ctx, document.Source{
-		URI: "./information/mysql-1.md",
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	docs, err := r.Splitter.Transform(ctx, doc)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, d := range docs {
-		uuid, _ := uuid2.NewUUID()
-		d.ID = uuid.String()
-	}
-
-	err = r.InitVectorIndex(ctx)
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = r.Indexer.Store(ctx, docs)
-	if err != nil {
-		panic(err)
-	}
-
-	//var query string
-	//for {
-	//	_, _ = fmt.Scan(&query)
-	//	output, err := r.Generate(ctx, query)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	var fullContent string // 用来拼接所有片段
-	//	for {
-	//		o, err := output.Recv()
-	//		if err != nil {
-	//			if err == io.EOF {
-	//				break
-	//			}
-	//			panic(err) // 其他错误才 panic
-	//		}
-	//		if o.Content != "" {
-	//			fullContent += o.Content
-	//			fmt.Print(o.Content)
-	//		}
-	//	}
-	//}
-}
+//func save(ctx context.Context) {
+//	r, err := demo_rag.InitRAGEngine(ctx, index, prefix)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	doc, err := r.Loader.Load(ctx, document.Source{
+//		URI: "./information/mysql-1.md",
+//	})
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	docs, err := r.Splitter.Transform(ctx, doc)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	for _, d := range docs {
+//		uuid, _ := uuid2.NewUUID()
+//		d.ID = uuid.String()
+//	}
+//
+//	err = r.InitVectorIndex(ctx)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	_, err = r.Indexer.Store(ctx, docs)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	//var query string
+//	//for {
+//	//	_, _ = fmt.Scan(&query)
+//	//	output, err := r.Generate(ctx, query)
+//	//	if err != nil {
+//	//		panic(err)
+//	//	}
+//	//	var fullContent string // 用来拼接所有片段
+//	//	for {
+//	//		o, err := output.Recv()
+//	//		if err != nil {
+//	//			if err == io.EOF {
+//	//				break
+//	//			}
+//	//			panic(err) // 其他错误才 panic
+//	//		}
+//	//		if o.Content != "" {
+//	//			fullContent += o.Content
+//	//			fmt.Print(o.Content)
+//	//		}
+//	//	}
+//	//}
+//}
